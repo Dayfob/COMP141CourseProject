eec1ddfdd5fa3e5285f7cc5af6ef9d286c5486fd
Henglay-Eung <eunghenglay@gmail.com>
2024-02-10 14:36:56 -0800

    Read and write file

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,32 @@
 // Henglay Eung
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            FileReader reader = new FileReader(System.getProperty("user.dir") + "/src/main/java/test_input.txt");
+            System.out.println(System.getProperty("user.dir"));
+            FileReader reader = new FileReader("src/test_input.txt");
             BufferedReader bufferedReader = new BufferedReader(reader);
             String line;
 
-//            while ((line = bufferedReader.readLine()) != -1) {
-//                System.out.println(line);
-//            }
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
             reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter writer = new FileWriter("src/test_out.txt", true);
+            writer.write("Hello World");
+            writer.write("\r\n");   // write new line
+            writer.write("Good Bye!");
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
