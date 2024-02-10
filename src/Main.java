24d200a3fdbc417e81325925b36acbbe018b9517
Dayfob <semembaevalihan19@gmail.com>
2024-02-10 14:22:27 -0800

    init

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Phase 1.1
+// Alikhan Semembayev
+// Henglay Eung
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            FileReader reader = new FileReader(System.getProperty("user.dir") + "/src/main/java/test_input.txt");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//            while ((line = bufferedReader.readLine()) != -1) {
+//                System.out.println(line);
+//            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
 }
---
+++ src/Scanner.java
@@ -0,0 +1,18 @@
+// Phase 1.1
+// Alikhan Semembayev
+// Henglay Eung
+public class Scanner {
+    String InputString;
+    String[] tokens;
+
+
+    public Scanner(String input) {
+        this.InputString = input;
+    }
+
+    public String[] getTokens() {
+
+
+        return this.tokens;
+    }
+}
